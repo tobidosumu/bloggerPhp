0e966be0e7594a5a2bbc546898844ae91f5d89b5
tobidosumu <dosunmumishael@gmail.com>
2023-01-29 23:21:53 +0100

    implemented cart page frontend

--- cart.php
+++ cart.php
@@ -37,13 +37,13 @@
     <div class="mainContainer"> <!-- contains all the page contents -->
         
         <?php include $userLoggedIn ? './headers/cartHeader.php' : './headers/notLoggedInCartHeader.php'; ?>
-
-        <section class="hero border-bottom">
-
-            <section class="caption">
+            
+        <section class="cartContents">
+            
+            <section class="cartContentsContainer"> <!-- blog contents container starts here -->
 
                 <?php if ($userLoggedInViewContents):?> <!--  Do not display post contents to a user who is not logged in -->
-                        
+                    
                     <div class="notLoggedIndashboardView mb-3 d-flex justify-content-around flex-column">
                         <div class="d-flex justify-content-around">
                             <a href="./login.php" class="actionBtn rounded-1">Cart</a> 
@@ -51,37 +51,83 @@
                         </div>
                         <p class="m-auto mt-3">Sign up or Login to see all your posts and posts you've liked.</p>
                     </div>
-
+    
                     <?php else: ?>
+    
+                    <div class="loggedInCartContents d-flex justify-content-between"> <!--- Signed in Dashboard -->
+                        <main class="border border-end-0">
+                            <div class="contentTitle d-flex justify-content-between align-items-center border-bottom pb-3 mb-3">
+                                <h3>Shopping Cart</h3>
+                                <h5>3 Items</h5>
+                            </div>
 
-                    <div class="dashboardView d-flex justify-content-center align-items-center"> <!--- Signed in Dashboard -->
-                        <div class="userProfile d-flex flex-column align-items-center">
-                            <div class="userProfileAvatar d-flex justify-content-between align-items-center">
-                                <img src="./assets/images/moji.png" alt="user avatar">
-                                <p class="likeCount p-1 pt-2 px-2"><i class="bi bi-heart-fill"></i> 1.4M</p>
+                            <table class="table table-borderless">
+                                <thead>
+                                    <tr>
+                                      <th scope="col">Product Details</th>
+                                      <th scope="col" class="text-center">Quantity</th>
+                                      <th scope="col" class="text-center">Price</th>
+                                      <th scope="col" class="text-center">Total</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                      <td class="d-flex">
+                                        <div class="productPhoto me-3">
+                                            <img src="./assets/images/addidas.webp" alt="product photo">
+                                        </div>
+                                        <div class="productInfo d-flex flex-column justify-content-around">
+                                            <h6 class="d-inline-block text-truncate">Adidas Vs Pace Lifestyle</h6>
+                                            <p><a href="#">Shoes</a></p>
+                                            <i class="bi bi-trash3"></i>
+                                        </div>
+                                      </td>
+
+                                      <td class="productQuantity text-center">
+                                        <div class="d-flex justify-content-around align-items-center">            
+                                            <button onclick="decrement()">-</button>         
+                                            <h2 id="counting">1</h2>
+                                            <button onclick="increment()">+</button>  
+                                        </div>
+                                      </td>
+                                      <td class="productPrice text-center">₦ 29,978</td>
+                                      <td class="totalPrice text-center">₦ 29,978</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </main>
+                        <aside class="border">
+                            <div class="contentTitle d-flex justify-content-between align-items-center border-bottom">
+                                <h3>Order Summary</h3>
                             </div>
-                            <p class="mt-2">Mojisola Badmus</p>
-                            <div class="userProfileInfo d-flex justify-content-between">
-                                <p>Followers 214</p>
-                                <i class="bi bi-dot"></i>
-                                <p>Email: moji@gmail.com</p>
+                            <div class="orderSummary d-flex justify-content-between">
+                                <p>Items 3</p>
+                                <p>₦ 29,978</p>
                             </div>
-                        </div>
+                            <div class="orderShipping d-flex flex-column pt-3">
+                                <p>Shipping</p>
+                                <select class="form-select form-select-sm mt-3" aria-label=".form-select-sm example">
+                                    <option selected>Standard Delivery - ₦500</option>
+                                    <option value="1500">Express Delivery - ₦2,500</option>
+                                    <option value="1500">Delivery By Mail - ₦3,500</option>
+                                </select>
+                            </div>
+                            <div class="promoCode pt-3">
+                                <p class="pb-3">Promo code</p>
+                                <input type="text" class="rounded-1" placeholder="Enter your code">
+                                <button type="submit" class="mt-4 border-0 rounded-1">Apply</button>
+                            </div>
+                            <div class="orderFooter pt-4 mt-5 border-top">
+                                <div class="totalCost d-flex justify-content-between">
+                                    <p>Total cost</p>
+                                    <p>₦ 29,978</p>
+                                </div>
+                                <button type="submit" class="mt-4 border-0 rounded-1">Checkout</button>
+                            </div>
+                        </aside>
                     </div>
-
+    
                 <?php endif; ?> 
-
-            </section>
-
-        </section>
-
-        <section class="blogContents pt-5">
-            
-            <section class="blogContentContainer"> <!-- blog contents container starts here -->
-
-                <section class="blogPostContainer">
-         
-                </section>
 
             </section>
 
